7161f08dd8ff61df7a31616238739e68de27042c
Samantha Juntilla <juntilla@hawaii.edu>
2019-01-16 17:11:00 -1000

    Attempts different methods.

--- linkedList.js
+++ linkedList.js
@@ -3,6 +3,53 @@
  * @description  Main Module
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
-function linkedListGenerator(){
+function linkedListGenerator() {
 
-}
+    let head = null;
+    let tail = null;
+
+    const getHead = () => {
+        return head;
+    };
+    const getTail = () => {
+        return tail;
+    };
+    const add = (val) => {
+        let newN = {
+            value: val,
+            next: null
+        };
+
+        if (!head) {
+            head = newN;
+            tail = newN;
+        } else {
+            tail.next = newN;
+            tail = newN;
+        }
+        return newN;
+    };
+
+    const get = (num) => {
+        let node = getHead();
+
+    };
+
+
+    const remove = (num) => {
+
+    };
+
+    const insert = (val, num) => {
+
+    };
+
+    return methods = {
+        getHead,
+        getTail,
+        add,
+        get,
+        remove,
+        insert
+    }
+};
